d696cb11d1af04257967d75e8b0b019969f82f8a
michael david <michaeldavid.cifuentes@gmail.com>
2019-02-26 00:17:14 -0500

    Pantallas 2

---
+++ app/src/main/java/com/example/proyectomascota/OlvidadContrasenaConfirmacion.java
@@ -0,0 +1,13 @@
+package com.example.proyectomascota;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OlvidadContrasenaConfirmacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_olvidad_contrasena_confirmacion);
+    }
+}
