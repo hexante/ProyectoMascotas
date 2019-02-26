5773899062a6d9868d7ee062f61f456b005245cb
michael david <michaeldavid.cifuentes@gmail.com>
2019-02-26 00:07:09 -0500

    Pantallas

---
+++ app/src/main/java/com/example/proyectomascota/RegitroFundacion.java
@@ -0,0 +1,13 @@
+package com.example.proyectomascota;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegitroFundacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regitro_fundacion);
+    }
+}
