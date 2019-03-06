7f38e653a08c1aa78b66ab7c765f0b5bdb87e4f3
michael <michaeldavid.cifuentes@gmail.com>
2019-03-06 15:51:27 -0500

    registrar correo a firebase

--- app/src/main/java/com/example/proyectomascota/IngresoUsuario.java
+++ app/src/main/java/com/example/proyectomascota/IngresoUsuario.java
@@ -1,119 +1,38 @@
 package com.example.proyectomascota;
 
+
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.facebook.AccessToken;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FacebookAuthProvider;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import android.view.View;
+import android.widget.Button;
 
 
 public class IngresoUsuario extends AppCompatActivity {
 
-    private CallbackManager mCallbackManager;
-    private FirebaseAuth mAuth;
 
-    private static final String TAG = "FACELOG";
+    Button btnregitro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingreso_usuario);
 
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
+        btnregitro = (Button) findViewById(R.id.btninicioregistro);
 
-        // Initialize Facebook Login button
-        mCallbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setReadPermissions("email", "public_profile");
-        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+        btnregitro.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSuccess(LoginResult loginResult) {
-                Log.d(TAG, "facebook:onSuccess:" + loginResult);
-                handleFacebookAccessToken(loginResult.getAccessToken());
-            }
+            public void onClick(View v) {
 
-            @Override
-            public void onCancel() {
-                Log.d(TAG, "facebook:onCancel");
-                // ...
-            }
+                Intent IngreoUsuario = new Intent(getApplicationContext(), RegistroUsuario.class);
+                // IngreoUsuario.putExtra("usuario",usuario);
+                //  IngreoUsuario.putExtra("fundacion",fundacion);
+                startActivity(IngreoUsuario);
 
-            @Override
-            public void onError(FacebookException error) {
-                Log.d(TAG, "facebook:onError", error);
-                // ...
             }
         });
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser != null){
-            updateUI();}
-    }
-
-    private void updateUI() {
-        Toast.makeText(IngresoUsuario.this, "Ingresaste", Toast.LENGTH_LONG).show();
-
-        Intent EntraUsuario = new Intent(IngresoUsuario.this,PerfilUsurio.class);
-        startActivity(EntraUsuario);
-        finish();
-    }
 
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Pass the activity result back to the Facebook SDK
-        mCallbackManager.onActivityResult(requestCode, resultCode, data);
-    }
-
-    private void handleFacebookAccessToken(AccessToken token) {
-        Log.d(TAG, "handleFacebookAccessToken:" + token);
-
-        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            Toast.makeText(IngresoUsuario.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI();
-                        }
-
-                        // ...
-                    }
-                });
     }
 
 
--- app/src/main/java/com/example/proyectomascota/RegistroUsuario.java
+++ app/src/main/java/com/example/proyectomascota/RegistroUsuario.java
@@ -1,13 +1,96 @@
 package com.example.proyectomascota;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistroUsuario extends AppCompatActivity {
+
+    private EditText txtemail;
+    private EditText txtcontraseña;
+    private Button btnregistro;
+    private ProgressDialog progressDialog;
+
+    //declaramos un objeto firebase
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario);
+
+        //iniciamos el objeto firebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //referenciamos los views
+
+        txtemail = (EditText) findViewById(R.id.txtcorreo);
+        txtcontraseña = (EditText) findViewById(R.id.txtContrasena);
+
+        btnregistro = (Button) findViewById(R.id.btnregistro);
+
+        progressDialog = new ProgressDialog(this);
+
+
+        //
+        btnregistro.setOnClickListener((View.OnClickListener) this);
+    }
+    private void registrarUsuario (){
+
+        //obtenemos el correo y contraseña desde la caja de texto
+        String email = txtemail.getText().toString().trim();
+        String contraseña = txtcontraseña.getText().toString().trim();
+
+        //verificamos que las cajas no esten vacias
+        if(TextUtils.isEmpty(email)){//igual que .equals
+
+            Toast.makeText(this,"Se debe ingresar un email",Toast.LENGTH_LONG).show();
+            return;        }
+        if (TextUtils.isEmpty(contraseña)){
+
+            Toast.makeText(this,"se debe ingresar una contraseña",Toast.LENGTH_LONG).show();
+            return;        }
+
+        progressDialog.setMessage("Realizando Registro En Linea...");
+        progressDialog.show();
+
+
+
+        firebaseAuth.createUserWithEmailAndPassword(email,contraseña)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //chaquiamos if correcto
+                        if (task.isSuccessful()){
+
+                            Toast.makeText(RegistroUsuario.this,"Se  ha registrado email",Toast.LENGTH_LONG).show();
+                        }else{
+
+                            Toast.makeText(RegistroUsuario.this,"No se pudo registrar el usuario",Toast.LENGTH_LONG).show();
+                        }
+
+                        progressDialog.dismiss();
+                    }
+                });
+
+
+    }
+
+
+    public void onClick(View view) {
+        //invocamos el metodo registro
+        registrarUsuario();
     }
 }
